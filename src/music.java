2041d99f02a5e3c41a88a49158ccb932f9885f45
Charles Crossan <crossan007@gmail.com>
2023-03-30 19:34:56 -0400

    initial commit

---
+++ src/music.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author fisherj
+ *
+ */
+public class music {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}

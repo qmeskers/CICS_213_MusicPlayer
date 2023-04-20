29131f621c0bf9f5eee0fdeddd5424d38f9d97e5
qmeskers <qmeskers@gmail.com>
2023-04-19 22:29:28 -0400

    adding Dominiks tree gui element and undoing tabs

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.wb.swt.SWTResourceManager;
@@ -14,57 +18,82 @@ import org.eclipse.swt.widgets.Control;
 
 
 public class Player extends Shell {
-	/*for the timer we might want to do something like timer += 2 after we set it when the video loads to account for load 
-	times in the youtube player so the song doesn't cut off early */
-	private int timer = 5; //this number will be replaced with the song length, set to five for testing purposes
-	private int stopTime; //currently unused, will be used for pause button
-	private boolean paused = false; //currently unused, variable will be used to stop timer when song is paused
-	private String currentSong = "https://www.youtube.com/embed/Zmvt7yFTtt8?autoplay=1"; //will be replaced by top of queue 
-	private String testURL = "https://www.youtube.com/embed/MgV-bCxE6ZI?autoplay=1"; //used for testing purposes
-	private ArrayList<Song> songlist = new ArrayList<Song>();// this is the arraylist for the song list-brian
-	
-	
-	  private Song song;
-	    
-	    public Player(String artist, String album, String name, String url, int yearReleased, String genre, int duration) {
-	        this.song = new Song(artist, album, name, url, yearReleased, genre, duration);
-	    }
-	    
+    /*for the timer we might want to do something like timer += 2 after we set it when the video loads to account for load 
+    times in the youtube player so the song doesn't cut off early */
+    private int timer = 5; //this number will be replaced with the song length, set to five for testing purposes
+    private int stopTime; //currently unused, will be used for pause button
+    private boolean paused = false; //currently unused, variable will be used to stop timer when song is paused
+    private String currentSong = "https://www.youtube.com/embed/Zmvt7yFTtt8?autoplay=1"; //will be replaced by top of queue 
+    private String testURL = "https://www.youtube.com/embed/MgV-bCxE6ZI?autoplay=1"; //used for testing purposes
+    private java.util.List<Song> songList = new ArrayList<Song>();// this is the arraylist for the song list-brian
+    private Song song;
 
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
-	public static void main(String args[]) {
-		try {
-			Display display = Display.getDefault();
-			Player shell = new Player(display);
-			shell.open();
-			shell.layout();
-			while (!shell.isDisposed()) {
-				if (!display.readAndDispatch()) {
-					display.sleep();
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public Player(String artist, String album, String name, String url, int yearReleased, String genre, int duration) {
+        this.song = new Song(artist, album, name, url, yearReleased, genre, duration);
+    }
 
-	/**
-	 * Create the shell.
-	 * @param display
-	 * @throws IOException 
-	 */
-	public Player(Display display) throws IOException {
-		super(display, SWT.SHELL_TRIM);
-		setMaximumSize(new Point(600, 400));
-		setMinimumSize(new Point(600, 400));
-		  // load the songs from the JSON file
-	   
-	   
-		
-	
+    /**
+     * Launch the application.
+     * @param args
+     */
+    public static void main(String args[]) {
+        try {
+            Display display = Display.getDefault();
+            Player shell = new Player(display);
+            shell.open();
+            shell.layout();
+            while (!shell.isDisposed()) {
+                if (!display.readAndDispatch()) {
+                    display.sleep();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    /**
+     * Create the shell.
+     * @param display
+     * @throws IOException 
+     */
+    public Player(Display display) throws IOException {
+        super(display, SWT.SHELL_TRIM);
+        setMaximumSize(new Point(600, 400));
+        setMinimumSize(new Point(600, 400));
+        
+     // load the songs from the JSON file
+        songList = Song.loadSongsFromJson("songsList.json");
+
+        // create the hashmap to store songs by genre
+        HashMap<String, ArrayList<Song>> songsByGenre = new HashMap<>();
+
+        for (Song song : songList) {
+            String genre = song.getGenre();
+            if (!songsByGenre.containsKey(genre)) {
+                songsByGenre.put(genre, new ArrayList<Song>());
+            }
+            songsByGenre.get(genre).add(song);
+        }
+
+        // create the tree widget
+        Tree tree = new Tree(this, SWT.BORDER);
+        tree.setBounds(285, 20, 150, 200);
+
+        // populate the tree with genres as top-level items
+        for (String genre : songsByGenre.keySet()) {
+            TreeItem genreItem = new TreeItem(tree, SWT.NONE);
+            genreItem.setText(genre);
+
+            // add the songs for this genre as children of the genre item
+            for (Song song : songsByGenre.get(genre)) {
+                TreeItem songItem = new TreeItem(genreItem, SWT.NONE);
+                songItem.setText(song.getName());
+                songItem.setData(song);
+            }
+        }
+
 		Label lblArtist = new Label(this, SWT.NONE);
 		lblArtist.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
 		lblArtist.setBounds(10, 10, 81, 25);
@@ -96,9 +125,47 @@ public class Player extends Shell {
 		btnRepeat.setBounds(36, 279, 105, 35);
 		btnRepeat.setText("Repeat");
 		
+		tree.addSelectionListener(new SelectionAdapter() {
+	        @Override
+	        public void widgetSelected(SelectionEvent event) {
+	            // Get the selected item in the tree
+	            TreeItem selectedItem = (TreeItem) event.item;
+	            
+	            // Get the Song object associated with the selected item
+	            Song selectedSong = (Song) selectedItem.getData();
+	            
+	            // Update the currentSong URL if a song is selected
+	            if (selectedSong != null) {
+	                currentSong = selectedSong.getUrl();
+	                lblWhenSongPlays.setText(selectedSong.getArtist());
+	                lblWhenSongPlays_1.setText(selectedSong.getAlbum());
+	                lblNewLabel.setText(selectedSong.getName());
+	                  
+	            }
+	        }
+	    });
 		Button btnPlay = new Button(this, SWT.NONE);
 		btnPlay.setBounds(168, 279, 105, 35);
 		btnPlay.setText("Play");
+		
+		btnPlay.addSelectionListener(new SelectionAdapter() {
+		    @Override
+		    public void widgetSelected(SelectionEvent e) {
+		        // Get the selected item in the tree
+		        TreeItem selectedItem = tree.getSelection()[0];
+		        
+		        // Get the Song object associated with the selected item
+		        Song selectedSong = (Song) selectedItem.getData();
+		        
+		        // Update the currentSong URL if a song is selected
+		        if (selectedSong != null) {
+		            currentSong = selectedSong.getUrl();
+		            System.out.println("Playing " + selectedSong.getUrl());
+		        }
+		    }
+		});
+		
+	
 		btnPlay.addListener(SWT.Selection, event -> {
 			/* creates the browser that has the embedded youtube video, made the size 1 by 1 so it is invisible. Anytime a
 			a new song gets loaded the startTimer method will need to be called */
@@ -115,9 +182,9 @@ public class Player extends Shell {
 		Button btnSkip = new Button(this, SWT.NONE);
 		btnSkip.setBounds(429, 279, 105, 35);
 		btnSkip.setText("Skip");
-		createContents();
-	}
-
+		createContents(); 
+}
+	
 	/**
 	 * Create contents of the shell.
 	 */
@@ -165,7 +232,7 @@ public class Player extends Shell {
 	                //creating the new browser
 	                Browser browser = new Browser(Player.this, SWT.NONE);
 	                browser.setBounds(50, 50, 1, 1);
-	                browser.setUrl(testURL);
+	                browser.setUrl(currentSong);
 	                // Set the timer variable to 36 for testing purposes will be changed later
 	                timer = 36;
 	                //start timer method will be called again here
